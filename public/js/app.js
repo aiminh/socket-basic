a8c9a8e22122105d4de397d4fba64109894b9626
Matthew Huang <mtt.hng@gmail.com>
2016-03-21 20:17:26 -0400

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -1,29 +1,35 @@
-var name = getQueryVariable('name')||'Anonymous';
-var room = getQueryVariable('room');
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room') || 'NOT SET';
 
 console.log(name + ' wants to join ' + room);
 
+//update h1 tag
+jQuery('.room-title').text(room);
+
 var socket = io();
 
-socket.on('connect', function () {
+socket.on('connect', function() {  
 	console.log('Conncted to socket.io server!');
 
+	socket.emit('joinRoom', {  //init an event, send to server with obj of my name and room name
+		name:name,
+		room:room
+	});
 });
 
-socket.on('message', function (message) {
-	console.log('New message:');
-	console.log(message.text);
+socket.on('message', function(message) {  //when receive event from server, message is obj created in server.js
+
+	// console.log('New message:');
+	// console.log(message.text);
 
 
 	var momentTimeStamp = moment.utc(message.timeStamp);
 
-	var $message = jQuery('.messages');   //$ means it is jQuery selector 
+	var $message = jQuery('.messages'); //$ means it is jQuery selector 
 
 	$message.append('<p><strong>' + message.name + ' ' + momentTimeStamp.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
 
-	$message.append('<p>' + message.text + '</p>' );
-
-	//jQuery('.messages').append('<p> <strong>' +  momentTimeStamp.local().format('h:mm a')  +'</strong>:  '  + message.text + '</p>' ) ;  //use . to target by class
 
 });
 
@@ -31,13 +37,13 @@ socket.on('message', function (message) {
 
 var $form = jQuery('#message-form'); //use $ because : just say its a variable that stores a jQuery instance of an element, meaning this variable has access to all methods that u could access any jQuery element 
 
-$form.on('submit', function(event){
+$form.on('submit', function(event) {
 	event.preventDefault(); //no freshing entire page
 
 	var $message = $form.find('input[name=message]');
 
-	socket.emit('message', {
-		name:name,
+	socket.emit('message', {  //init event, and send to server
+		name: name,
 		text: $message.val()
 	});
 
--- server.js
+++ server.js
@@ -7,31 +7,46 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
 
+var clientInfo = {}; //global container to store user name and room
+
+app.use(express.static(__dirname + '/public'));
+
+var now = moment();
+
+var momentTimeStamp = now.valueOf();
 
 
-app.use(express.static(__dirname + '/public') );
-
-	var now = moment();
-
-	var momentTimeStamp =  now.valueOf();
-
-
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+	
 	console.log('User connected via socket.io!');
 
-	socket.on('message', function(message){
-		console.log('Messaged received:'+ message.text);
-		//console.log('Messaged received:'+ message.timeStamp);
+	socket.on('joinRoom', function(req) { //req is the obj {name, room} created in joinRoom event in app.js
 
-		//socket.broadcast.emit('message', message)   //send to every browser connected to server except myself
-		
-		message.timeStamp = moment().valueOf();
+		clientInfo[socket.id] = req;  //socket.id is where socket.io store unique identifier, store info(current user, room) in socket.id
 
-		io.emit('message', message)   //send to every browser connected to server
+		socket.join(req.room);   //join is built-in method, tells socket.io libaray to add this socket to this room
+
+		socket.broadcast.to(req.room).emit('message', {  //send the message obj to browser with this 'room'
+			name: 'System',
+			text: req.name + ' has joined!',
+			timeStamp: moment().valueOf()
+		});   //'to' send to specific room
 
 	});
 
-	socket.emit('message', {
+	socket.on('message', function(message) { //when receive event from browser
+		console.log('Messaged received:' + message.text);
+
+		//socket.broadcast.emit('message', message)   //send to every browser connected to server except myself
+
+		message.timeStamp = moment().valueOf();
+
+		//io.emit('message', message) //send to every browser connected to server
+		io.to(clientInfo[socket.id].room).emit('message', message); //only send to the browers with the room which current user is in
+
+	});
+
+	socket.emit('message', { //init event and send to browser connected to this server
 		name: 'System',
 		text: 'Welcome to the chat application!',
 		timeStamp: moment().valueOf()
@@ -39,6 +54,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(PORT, function(){
+http.listen(PORT, function() {
 	console.log('Server started');
 });
